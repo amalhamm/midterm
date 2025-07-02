db22b33698137f2c7e4bffdf33215e9e3681ff21
Amal Hammoud <amalhamm@umich.edu>
2025-07-02 12:06:20 -0400

    Java added

---
+++ quarticsolver.java
@@ -0,0 +1,13 @@
+public class quraticsolver {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = -3;
+        int c = 2;
+
+        double discriminant = b * b - 4 * a * c;
+        double root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        System.out.println("The roots are: " + root1 + " and " + root2);
+    }
+}
